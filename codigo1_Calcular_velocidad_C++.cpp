be3935d073e56ccae7baa0101cd5a641b7c2a1ff
HenryHerrera15 <henryconcoha15@gmail.com>
2018-03-22 18:55:04 -0600

    primer commit

---
+++ codigo1_Calcular_velocidad_C++.cpp
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+int main(){
+
+
+ float velocidad, distancia, tiempo;
+
+printf("Ingrese la distancia recorrida: ");
+scanf("%f", &distancia);
+printf("Ingrese el tiempo recorrido; ");
+scanf("%f", &
+tiempo);
+
+velocidad = distancia / tiempo;
+
+
+printf ("La velocidad es de %f k/h", velocidad);
+}
+
---
+++ codigo2/codigo2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codigo2
+{
+    class Program
+    {
+        const double gravedad = 9.8;
+        static void Main(string[] args)
+        {
+            double peso, masa;
+
+            Console.WriteLine("Ingrese la masa: ");
+            masa = double.Parse(Console.ReadLine());
+
+            peso = masa * gravedad;
+
+
+            Console.WriteLine("El Peso es de: " + peso + " Newton");
+
+            Console.ReadKey();
+
+        }
+    }
+}
